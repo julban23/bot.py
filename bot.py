9d78959a1d63f48c0c4a3b2491fcf9db9b1268aa
julban23 <71963228+julban23@users.noreply.github.com>
2025-04-01 09:43:38 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import logging
 import requests
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram.ext import Updater, CommandHandler, MessageHandler, filters, CallbackContext
 
 # Включаем логирование
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
